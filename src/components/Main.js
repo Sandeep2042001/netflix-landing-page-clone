b40034eb1542c7c4780803caa423a2db1eb5b5b4
Ayush Saini <ayushsainiiitb00@gmail.com>
2021-06-03 21:42:39 +0530

    animation sections

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,12 @@
 import AnimationCard from './subComponents/AnimationCard';
 import FAQ from './subComponents/FAQ';
+import tv from '../assets/tv.png';
+import mobile from '../assets/mobile.jpg';
+import devicePile from '../assets/device-pile-in.png';
+import child from '../assets/children.png';
+import videoTv from '../assets/video-tv-in-0819.m4v';
+import videoDevicePile from '../assets/video-devices-in.m4v';
+import downloadIcon from '../assets/download-icon.gif';
 
 const Main = () => {
   return (
@@ -8,31 +15,34 @@ const Main = () => {
         <AnimationCard
           title="Enjoy on your TV."
           subtitle="Watch on smart TVs, PlayStation, Xbox, Chromecast, Apple TV, Blu-ray players and more."
-          image="tv.png"
+          image={tv}
           imageAlt="tv"
+          videoTv={videoTv}
         />
       </section>
       <section className="download-your-shows-section">
         <AnimationCard
           title="Download your shows to watch offline."
           subtitle="Save your favourites easily and always have something to watch."
-          image="mobile.png"
+          image={mobile}
           imageAlt="mobile"
+          downloadIcon={downloadIcon}
         />
       </section>
       <section className="watch-everywhere-section">
         <AnimationCard
           title="Watch everywhere."
           subtitle="Stream unlimited movies and TV shows on your phone, tablet, laptop, and TV."
-          image="device-pile-in.png"
+          image={devicePile}
           imageAlt="device pile in"
+          videoDevicePile={videoDevicePile}
         />
       </section>
       <section className="children-section">
         <AnimationCard
           title="Create profiles for children."
           subtitle="Send children on adventures with their favourite characters in a space made just for them—free with your membership."
-          image="children.png"
+          image={child}
           imageAlt="children"
         />
       </section>
--- src/components/subComponents/AnimationCard.js
+++ src/components/subComponents/AnimationCard.js
@@ -1,11 +1,70 @@
-const AnimationCard = ({ title, subtitle, image, imageAlt }) => {
+import strangerThings from '../../assets/boxshot.png';
+
+const AnimationCard = ({
+  title,
+  subtitle,
+  image,
+  imageAlt,
+  videoTv,
+  videoDevicePile,
+  downloadIcon
+}) => {
   return (
     <div className="animation-card">
-      <h1>{title}</h1>
-      <h2>{subtitle}</h2>
-      <div className="card-image">
-        <img src={`../assets/${image}`} alt={imageAlt} />
-        <div className="animation"></div>
+      <div className="animation-card-text-wrapper">
+        <h1 className="animation-card-title">{title}</h1>
+        <h2 className="animation-card-sub-title">{subtitle}</h2>
+      </div>
+      <div className="animation-card-image-wrapper">
+        <img className="animation-card-image" src={image} alt={imageAlt} />
+        <div className="animation">
+          {videoTv && (
+            <video
+              className="animation-card-video video-tv"
+              autoPlay
+              playsInline
+              muted
+              loop
+            >
+              <source src={videoTv} type="video/mp4" />
+            </video>
+          )}
+          {downloadIcon && (
+            <div className="animation-card-icon">
+              <div className="animation-card-icon-image-wrapper">
+                <img
+                  className="animation-card-icon-image"
+                  src={strangerThings}
+                  alt="stranger things"
+                />
+              </div>
+              <div className="animation-card-icon-text-wrapper">
+                <div className="animation-card-icon-text-st">
+                  Stranger Things
+                </div>
+                <div className="animation-card-icon-text-d">Downloading...</div>
+              </div>
+              <div className="animation-card-icon-icon-wrapper">
+                <img
+                  className="animation-card-icon-icon-gif"
+                  src={downloadIcon}
+                  alt="download animation"
+                />
+              </div>
+            </div>
+          )}
+          {videoDevicePile && (
+            <video
+              className="animation-card-video video-device-pile"
+              autoPlay
+              playsInline
+              muted
+              loop
+            >
+              <source src={videoDevicePile} type="video/mp4" />
+            </video>
+          )}
+        </div>
       </div>
     </div>
   );
